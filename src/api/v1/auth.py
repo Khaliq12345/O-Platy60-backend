633757fd8ca8084ccb45832e2d0a8e517f7193fb
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-09-30 08:40:22 +0100

    auth modified

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -4,6 +4,8 @@ from src.schemas import auth_schema
 router = APIRouter(prefix="/v1/auth", tags=["AUTH"])
 
 
-@router.get("/login")
-def login(email: str, password: str) -> auth_schema.Login:
-    return auth_schema.Login(email=email, password=password)
+@router.post("/login")
+def login(credentials: auth_schema.Login) -> auth_schema.Login:
+    return auth_schema.Login(
+        email=credentials.email, password=credentials.password
+    )
