d1837082c92e4e65366122b8d41b15f41b0ae4ae
Eldo-Moréo GBOHOUILI <eldomoreogbohouil@gmail.com>
2025-10-21 12:53:27 +0100

    correction des optionnal et ajout d'ingredient dans dependecy

--- src/schemas/ingredients_schema.py
+++ src/schemas/ingredients_schema.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 from datetime import datetime
 
 
 class Ingredient(BaseModel):
     sku: str
     name: str
-    category: str | None = None
-    current_stock_level: float = 0.0
-    unit: str
-    status: str | None = None
-    min_stock_level: float = 0.0
-    storage_location: str | None = None
-    last_received: str | None = None
-    last_updated: str = datetime.now().isoformat()
+    category: Optional[str]
+    current_stock_level: float
+    unit: Optional[str]
+    status: Optional[str]
+    min_stock_level: float
+    storage_location: Optional[str]
+    last_received: Optional[datetime]
+    last_updated: datetime
     unit_cost: float
-    expire_at: str | None = None
-    delete: bool = False
-    value: float = 0
+    expire_at: Optional[datetime]
+    delete: bool
+    value: float
